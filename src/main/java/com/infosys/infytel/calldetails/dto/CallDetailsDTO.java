a2652fd1924b99007cf04299e97007d0b7646a0d
akshaygadikoppa <168340890+akshaygadikoppa@users.noreply.github.com>
2024-04-28 17:13:25 +0530

    Update CallDetailsDTO.java

--- src/main/java/com/infosys/infytel/calldetails/dto/CallDetailsDTO.java
+++ src/main/java/com/infosys/infytel/calldetails/dto/CallDetailsDTO.java
@@ -58,7 +58,7 @@ public class CallDetailsDTO {
 		this.duration = duration;
 	}
 
-	// Converts Entity into the  DTO class file // comments to trigger jenkin build
+	// Converts Entity  yu into the  DTO class file // comments to trigger jenkin build
 	public static CallDetailsDTO valueOf(CallDetails callDetails) {
 		CallDetailsDTO callsDTO = new CallDetailsDTO();
 		callsDTO.setCalledBy(callDetails.getCalledBy());
